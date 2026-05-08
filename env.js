7bcee1bf425e859bffd882a052f17a017065547f
jafongl <jose.fong1@gmail.com>
2026-05-07 21:26:29 -0500

    Add files via upload

---
+++ env.js
@@ -0,0 +1,4 @@
+// CajaLista — Configuración de staging
+// Este archivo va en la rama: dev
+window.__SB_URL__ = 'https://tmhxxxnwdonqfqgcnmcw.supabase.co';
+window.__SB_KEY__ = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtaHh4eG53ZG9ucWZxZ2NubWN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxOTI4MzYsImV4cCI6MjA5Mzc2ODgzNn0.asmxB6bTxfM1vu3kLZPnDXR_bN9mkk1brXNChEge18c';
