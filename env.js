ba5523f5eb727b7caf5e0dca65bc8ba8ce9d7519
jafongl <jose.fong1@gmail.com>
2026-05-07 21:25:44 -0500

    Add files via upload

---
+++ env.js
@@ -0,0 +1,4 @@
+// CajaLista — Configuración de producción
+// Este archivo va en la rama: main
+window.__SB_URL__ = 'https://lwnbjlcqxkoiirbsvcjr.supabase.co';
+window.__SB_KEY__ = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3bmJqbGNxeGtvaWlyYnN2Y2pyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczMjYyMjUsImV4cCI6MjA5MjkwMjIyNX0.1zDOKyWR8BxY-Gs1T3NlM6O94frskVzZJQxdaq8E0Mc';
