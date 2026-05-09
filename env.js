f0d1e47a697449724ca5b07de7412947af5165b1
jafongl <jose.fong1@gmail.com>
2026-05-08 20:03:50 -0500

    Update env.js

--- env.js
+++ env.js
@@ -1,6 +1,4 @@
-
-// CajaLista — Configuración de producción
-// Este archivo va en la rama: main
-window.__SB_URL__ = 'https://lwnbjlcqxkoiirbsvcjr.supabase.co';
-window.__SB_KEY__ = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3bmJqbGNxeGtvaWlyYnN2Y2pyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczMjYyMjUsImV4cCI6MjA5MjkwMjIyNX0.1zDOKyWR8BxY-Gs1T3NlM6O94frskVzZJQxdaq8E0Mc';
-
+// CajaLista — Configuración de staging
+// Este archivo va en la rama: dev
+window.__SB_URL__ = 'https://tmhxxxnwdonqfqgcnmcw.supabase.co';
+window.__SB_KEY__ = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtaHh4eG53ZG9ucWZxZ2NubWN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxOTI4MzYsImV4cCI6MjA5Mzc2ODgzNn0.asmxB6bTxfM1vu3kLZPnDXR_bN9mkk1brXNChEge18c';
